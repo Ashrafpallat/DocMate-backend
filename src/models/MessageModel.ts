a1bad46a6bb90a576204b01af7fd749bd6ab90b3
Ashrafpallat <ashrafpallat123@gmail.com>
2024-12-20 19:51:20 +0530

    bug fixes

--- src/models/MessageModel.ts
+++ src/models/MessageModel.ts
@@ -1,10 +1,10 @@
 import { Document, Schema, Types } from "mongoose";
 
-
 export interface IMessage extends Document {
-  conversationId: Types.ObjectId; 
+  chatId: Types.ObjectId; 
   sender: Types.ObjectId; 
   senderRole: 'patient' | 'doctor'; 
+  receiver: Types.ObjectId; 
   content: string; 
   read: boolean; 
   delivered: boolean; 
@@ -12,13 +12,13 @@ export interface IMessage extends Document {
   updatedAt: Date; 
 }
 
-
 const MessageSchema = new Schema<IMessage>(
   {
-    conversationId: { type: Schema.Types.ObjectId, ref: "Conversation", required: true }, 
+    chatId: { type: Schema.Types.ObjectId, ref: "Chat", required: true }, 
     sender: { type: Schema.Types.ObjectId, required: true }, 
-    senderRole: { type: String, enum: ["patient", "doctor"], required: true },
-    content: { type: String, required: true },
+    senderRole: { type: String, enum: ["patient", "doctor"], required: true }, 
+    receiver: { type: Schema.Types.ObjectId, required: true }, 
+    content: { type: String, required: true }, 
     read: { type: Boolean, default: false }, 
     delivered: { type: Boolean, default: false }, 
   },
