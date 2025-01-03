4651f13a82fcdaf003ba73db76b3fc3f9a8a0490
Ashrafpallat <ashrafpallat123@gmail.com>
2025-01-03 22:33:42 +0530

    made chat page responsive

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import doctorRoutes from './routes/doctorRoutes';
 import patientRoutes from './routes/patientRoutes';
-import chatRoutes from './routes/ChatRoutes';
+import chatRoutes from './routes/chatRoutes';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import adminRouter from './routes/adminRoutes';
--- src/controllers/ChatController.ts
+++ src/controllers/ChatController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { chatService } from "../services/ChatService"; 
 import { CustomRequest } from "../interfaces/customRequest";
+import { chatService } from "../services/ChatService";
 
 
  class ChatController {
